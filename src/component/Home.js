cb17a7d750271d7c85e062f8b49d245b069ce94e
shriyah <shriya.hangloo@tekmindz.com>
2023-08-25 13:59:21 +0530

    first commit

---
+++ src/component/Home.js
@@ -0,0 +1,183 @@
+import React from 'react';
+import styles from './Home.module.scss';
+import call from '../assets/Vector-4.png';
+import head from '../assets/Vector-5.png';
+import banner1 from '../assets/image 1.png';
+import image2 from '../assets/image 2.png';
+import image3 from '../assets/image 3.png';
+import image4 from '../assets/image 4.png';
+import image5 from '../assets/image 5.png';
+import image6 from '../assets/image 6.png';
+import Line from '../assets/Line 8.png';
+import Group from '../assets/Group 32.png';
+import banner2 from '../assets/banner1.png';
+import banner3 from '../assets/banner2.png';
+import vector6 from '../assets/Vector6.png';
+import vector7 from '../assets/Vector7.png';
+import group5 from '../assets/Group 243.png';
+import  
+{head_img,
+category,
+category1,
+trending,
+NewProduct,
+Product,
+group} from './common'
+
+function Home() {
+    
+    return (
+        <div className={styles.home}>
+            <div className={styles.top_header}>
+                <h3>Welcome to Our Store Hiscraves</h3>
+                <div className={styles.header_right}>
+                <img src={call} alt='' />
+                <h4>Call Us: 1234567890</h4>
+                </div>
+                
+            </div>
+            <header className={styles.home_header}>
+                <img src={head} alt='' />
+                <div style={{marginLeft:"117px"}}>Logo Here</div>
+                <div className={styles.header_img}>
+                    {head_img?.map((data, index) => 
+                    <img key={index} src={data?.img} alt=''></img>
+                    )}
+                </div>
+            </header>
+            <img className={styles.banner} src={banner1} alt=''/>
+                <div className={styles.inner_home}>
+                    <div className={styles.inner_card}>
+                        {category?.map((data)=>
+                        <div className={styles.card}>
+                        <img src={data?.img}/>
+                        </div>
+                        )} 
+                    </div>
+                    <div className={styles.wrapper_cards}>
+                        <div className={styles.wrapper}>
+                        <img src={image2}/>
+                        <img src={image3}/>
+                        </div>
+                        <div className={styles.wrapper2}>
+                            <img src={image4}/>
+                            <img src={image6}/>
+                            <img src={image5}/>
+                        </div>
+                    </div>
+                    {trending?.map((trend) => (
+                    <div key={trend.title}>
+                        <div className={styles.category_title}>
+                            <img src={Line} alt='' />
+                            <h1>{trend.title}</h1>
+                            <img src={Line} alt='' />
+                        </div>
+                        <div className={styles.card_block}>
+                            <div className={styles.item_card}>
+                                {trend.items.map((data, index) => (
+                                    <div key={index} className={styles.item}>
+                                        <img src={data.img} alt='' />
+                                        <div className={styles.card_text}>
+                                            <p>{data?.text1}</p>
+                                            <p>{data?.text2}</p>
+                                            <p>{data?.price}</p>
+                                            <img src={Group} alt='' />
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                            <button className={styles.button}>VIEW ALL</button>
+                        </div>
+                    </div>
+                    ))}
+                </div>
+            <img className={styles.banner} src={banner2} alt='' />
+                <div className={styles.inner_home}>
+                {NewProduct?.map((trend) => (
+                    <div key={trend.title}>
+                        <div className={styles.category_title}>
+                            <img src={Line} alt='' />
+                            <h1>{trend.title}</h1>
+                            <img src={Line} alt='' />
+                        </div>
+                        <div className={styles.card_block}>
+                            <div className={styles.item_card}>
+                                {trend?.items.map((data, index) => (
+                                    <div key={index} className={styles.item}>
+                                        <img src={data?.img} alt='' />
+                                        <div className={styles.card_text}>
+                                            <p>{data?.text1}</p>
+                                            <p>{data?.text2}</p>
+                                            <p>{data?.price}</p>
+                                            <img src={Group} alt='' />
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                            <button className={styles.button}>VIEW ALL</button>
+                        </div>
+                    </div>
+                    ))}
+                <br/>
+                <div className={styles.card_block}>
+                        <div className={styles.item_card}>
+                            {Product.map((data)=>
+                            <div className={styles.product_item}>
+                                <img src={data.img} alt=''/>
+                                <img src={data.img1} alt=''/>
+                             </div>
+                            )}  
+                        </div> 
+                </div>
+            </div>
+            <img className={styles.banner} src={banner3} alt='' />
+             <div className={styles.inner_home}>
+                <div>
+                    <div className={styles.card_block}>
+                        <div className={styles.item_card}>
+                            {category1.map((data,index)=>
+                            <div className = {index === 0 ? styles.item2 : styles.item3}>
+                                <div className={styles.card_text2}>
+                                <img src={data?.img} alt=''/>
+                                <p>Hurry Up!</p>
+                                <h1>85% OFF</h1>
+                                <p>Sale</p>
+                                <button>SHOP NOW</button>
+                                </div>
+                             </div>
+                            )}  
+                        </div>  
+                    </div>
+                </div>
+            </div>
+            <div className={styles.ecllips}></div> 
+            <div className={styles.group_card}>
+               {group?.map((data)=>
+               <div className={styles.group_icon}>
+               <img src={data?.img}/>
+               </div>
+               )}
+            </div> 
+            <div className={styles.ecllips1}></div> 
+            <div className={styles.card_block}>
+            <div className={styles.bottom_card}>
+                <img src={vector6}/>
+                <img style={{float:"left"}} src={vector7}/>
+                <div className={styles.bottom_card_text}>
+                    <h1>Subscribe to get updates on exciting offers & deals</h1>
+                    <div >
+                    <input
+                        type='text'
+                        placeholder="Enter your email"
+                    />
+                    <button>Subscribe</button>
+                    </div>
+                </div>
+            </div>
+            <img src={group5}/>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
---
+++ src/component/common.js
@@ -0,0 +1,250 @@
+
+import vector1 from '../assets/Vector-2.png';
+import vector2 from '../assets/Vector-3.png';
+import vector3 from '../assets/Vector-1.png';
+import vector4 from '../assets/Vector.png';
+import diamond from '../assets/diamond 1.png';
+import discount from '../assets/discount 1.png';
+import group1 from '../assets/Group1.png';
+import group2 from '../assets/Group2.png';
+import group3 from '../assets/Group3.png';
+import group4 from '../assets/Group4.png';
+import Rectangle1 from '../assets/Rectangle1.png';
+import Rectangle2 from '../assets/Rectangle2.png';
+import Rectangle3 from '../assets/Rectangle3.png';
+import Rectangle4 from '../assets/Rectangle4.png';
+import group6 from '../assets/Group 50.png';
+import group7 from '../assets/Group 51.png';
+import group8 from '../assets/Group 52.png';
+import group9 from '../assets/Group 53.png';
+import group10 from '../assets/Group 54.png';
+import group11 from '../assets/Group 55.png';
+import group12 from '../assets/Group 245.png';
+import group13 from '../assets/Group 246.png';
+import group14 from '../assets/Group 247.png';
+import group15 from '../assets/Group 248.png';
+import group16 from '../assets/Group 249.png';
+import group17 from '../assets/Group 244.png';
+import group18 from '../assets/Group 56.png';
+import group19 from '../assets/Group 57.png';
+import group20 from '../assets/Group 58.png';
+import group21 from '../assets/Group 59.png';
+import group22 from '../assets/Group 60.png';
+import group23 from '../assets/Group 61.png';
+
+const head_img = [
+    { img: vector1 },
+    { img: vector2 },
+    { img: vector3 },
+    { img: vector4 },
+];
+
+const category = [
+    { img: group17 },
+    { img: group12 },
+    { img: group13 },
+    { img: group14 },
+    { img: group15 },
+    { img: group16 }
+   
+]
+
+const category1 = 
+[
+    { img: diamond },
+    { img: discount }
+]
+
+const trending = 
+[
+    {
+        title: "Trending T-Shirts",
+        items: [
+            {
+                img: Rectangle1,
+                text1:'Men Henley Neck',
+                text2:'Full Sleeve Red Wine',
+                price:'₹399 '
+            },
+            {
+                img: Rectangle2,
+                text1:'Men Henley Neck',
+                text2:'Full Sleeve Red Wine',
+                price:'₹399 '
+            },
+            {
+                img: Rectangle3,
+                text1:'Men Henley Neck',
+                text2:'Full Sleeve Red Wine',
+                price:'₹399 '
+            },
+            {
+                img: Rectangle4,
+                text1:'Men Henley Neck',
+                text2:'Full Sleeve Red Wine',
+                price:'₹399 '
+            },
+            {
+                img: Rectangle1,
+                text1:'Men Henley Neck',
+                text2:'Full Sleeve Red Wine',
+                price:'₹399 '
+            },
+            {
+                img: Rectangle2,
+                text1:'Men Henley Neck',
+                text2:'Full Sleeve Red Wine',
+                price:'₹399'
+            },
+            {
+                img: Rectangle3,
+                text1:'Men Henley Neck',
+                text2:'Full Sleeve Red Wine',
+                price:'₹399'
+            },
+            {
+                img: Rectangle4,
+                text1:'Men Henley Neck',
+                text2:'Full Sleeve Red Wine',
+                price:'₹399'
+            }
+        ]
+    },
+    {
+        title: "Featured Products",
+        items: [
+            {
+                img: Rectangle1,
+                text1:'Men Henley Neck',
+                text2:'Full Sleeve Red Wine',
+                price:'₹399 '
+            },
+            {
+                img: Rectangle2,
+                text1:'Men Henley Neck',
+                text2:'Full Sleeve Red Wine',
+                price:'₹399'
+            },
+            {
+                img: Rectangle3,
+                text1:'Men Henley Neck',
+                text2:'Full Sleeve Red Wine',
+                price:'₹399'
+            },
+            {
+                img: Rectangle4,
+                text1:'Men Henley Neck',
+                text2:'Full Sleeve Red Wine',
+                price:'₹399 '
+            }
+        ]
+    }
+];
+
+const NewProduct = 
+[
+    {
+        title: "New Product",
+        items: [
+            {
+                img: Rectangle1,
+                text1:'Men Henley Neck',
+                text2:'Full Sleeve Red Wine',
+                price:'₹399'
+            },
+            {
+                img: Rectangle2,
+                text1:'Men Henley Neck',
+                text2:'Full Sleeve Red Wine',
+                price:'₹399 '
+            },
+            {
+                img: Rectangle3,
+                text1:'Men Henley Neck',
+                text2:'Full Sleeve Red Wine',
+                price:'₹399 '
+            },
+            {
+                img: Rectangle4,
+                text1:'Men Henley Neck',
+                text2:'Full Sleeve Red Wine',
+                price:'₹399 '
+            }
+        ]
+    },
+    {
+        title: "Best Selling Products",
+        items: [
+            {
+                img: Rectangle1,
+                text1:'Men Henley Neck',
+                text2:'Full Sleeve Red Wine',
+                price:'₹399'
+            },
+            {
+                img: Rectangle2,
+                text1:'Men Henley Neck',
+                text2:'Full Sleeve Red Wine',
+                price:'₹399 '
+            },
+            {
+                img: Rectangle3,
+                text1:'Men Henley Neck',
+                text2:'Full Sleeve Red Wine',
+                price:'₹399 '
+            },
+            {
+                img: Rectangle4,
+                text1:'Men Henley Neck',
+                text2:'Full Sleeve Red Wine',
+                price:'₹399 '
+            }
+        ]
+    }
+];
+
+const Product =
+ [
+            {
+                img: group6,
+                img1: group18,
+            },
+            {
+                img: group7,
+                img1: group19,
+            },
+            {
+                img: group8,
+                img1: group20,
+            },
+            {
+                img: group9,
+                img1: group21,
+            },
+            {
+                img: group10,
+                img1: group22,
+            },
+            {
+                img: group11,
+                img1: group23,
+            }
+];
+
+const group =  
+[
+    { img: group1 },
+    { img: group2 },
+    { img: group3 },
+    { img: group4 }
+]
+
+export {
+  head_img,
+  category,
+  category1,
+  trending,
+  NewProduct,
+  Product,
+  group
+};
